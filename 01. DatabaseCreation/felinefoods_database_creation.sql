788d96847c2ca7c431c28f7f8190a5f7403d0ef9
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2025-12-31 14:15:06 -0500

    Add files via upload

---
+++ 01. DatabaseCreation/felinefoods_database_creation.sql
@@ -0,0 +1,18 @@
+-- Creating the database called felinefoods
+CREATE DATABASE felinefoods;
+
+
+-- Displaying all databases to ensure that the felinefoods database was created
+SHOW DATABASES;
+
+
+-- Displaying the currently selected database
+SELECT DATABASE();
+
+
+-- Selecting the felinefoods database as the main database to add tables and records in
+USE felinefoods;
+
+
+-- Displaying the currently selected database, should now be felinefoods
+SELECT DATABASE();
