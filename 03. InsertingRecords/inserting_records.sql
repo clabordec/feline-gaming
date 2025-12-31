41f3d7a48fe7636245352326088b6f46d3b52a24
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2025-12-31 15:24:32 -0500

    Update inserting_records.sql

--- 03. InsertingRecords/inserting_records.sql
+++ 03. InsertingRecords/inserting_records.sql
@@ -1,3 +1,4 @@
+-- Add reords into the customers table
 INSERT INTO customers (name, contact, address)
 VALUES
   ('Kody', '42 Feline Farm', '555-0100'),
@@ -5,16 +6,19 @@ VALUES
   ('Paws',   '99 Whisker Way',  '555-0102');
 	
 
+-- Add reords into the combos table
 INSERT INTO combos (name, price)
 VALUES
   ('Fish & Chips', 8.50),
   ('Sushi Bento', 12.00),
   ('Pasta Combo', 10.00);
 	
-	
+
+-- Add reords into the orders table
 INSERT INTO orders (customer_id, combo_id, order_time)
 VALUES
   (1, 3, '2025-08-10 12:30:00'),
   (2, 2, '2025-08-10 13:15:00'),
 
   (3, 1, '2025-08-11 19:00:00');
+
