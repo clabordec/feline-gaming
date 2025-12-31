54bb5a235b0b58196e8f98099cbca1fd7ebcc85e
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2025-12-31 14:41:12 -0500

    Add files via upload

---
+++ 02. TableCreation/table_creation.sql
@@ -0,0 +1,26 @@
+-- Create the customers table
+CREATE TABLE customers (
+	customer_id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(100) NOT NULL,
+	contact VARCHAR(20) NOT NULL,
+	address TEXT
+);
+
+
+-- Create the combos table
+CREATE TABLE combos (
+	combo_id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(100) NOT NULL,
+	price DECIMAL(4,2)
+);
+
+
+-- Create the orders table
+CREATE TABLE orders (
+	order_id INT PRIMARY AUTO_INCREMENT,
+	customer_id INT,
+	combo_id INT,
+	order_time DATE,
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+	FOREIGN KEY (combo_id) REFERENCES combos(combo_id)
+);
