53d0ea186ef32fc6a238c15abf9ad526d9c79e25
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2026-01-01 01:29:59 -0500

    Create gaming_database.sql

---
+++ 01. DatabaseCreation/gaming_database.sql
@@ -0,0 +1,22 @@
+-- Display all created databases
+SHOW DATABASES;
+
+
+-- Create the gaming databases
+CREATE DATABASE gaming;
+
+
+-- Verify the creation of the database
+SHOW DATABASES;
+
+
+-- Show which database is currently selected
+SELECT DATABASE();
+
+
+-- Select the gaming database
+USE gaming;
+
+
+-- Verify the selection
+SELECT DATABASE();
